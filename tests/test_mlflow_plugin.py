3e8939c29f63af984f6bddaf22d30f9374c8f98f
Francisco Aranda <francisco@recogn.ai>
2021-05-05 07:13:03 +0200

    chore: configure tests

--- tests/test_mlflow_plugin.py
+++ tests/test_mlflow_plugin.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 
 import mlflow.pyfunc
@@ -6,6 +7,9 @@ from mlflow.deployments import get_deploy_client
 
 
 # Define the model class
+from mlflow.exceptions import MlflowException
+
+
 class AddN(mlflow.pyfunc.PythonModel):
     def __init__(self, n):
         self.n = n
@@ -15,20 +19,38 @@ class AddN(mlflow.pyfunc.PythonModel):
 
 
 # Construct and save the model
-model_path = "/Users/archit/mlflow-ray-serve/add_n_model"
+model_5_path = os.path.abspath("models/add_5_model")
 add5_model = AddN(n=5)
-mlflow.pyfunc.save_model(path=model_path, python_model=add5_model)
+try:
+    mlflow.pyfunc.save_model(path=model_5_path, python_model=add5_model)
+except MlflowException:
+    pass
+
+model_6_path = os.path.abspath("models/add_6_model")
+add6_model = AddN(n=6)
+try:
+    mlflow.pyfunc.save_model(path=model_6_path, python_model=add6_model)
+except MlflowException:
+    pass
 
 # Evaluate the model
 client = get_deploy_client("ray-serve")
 
-client.create_deployment("add5", model_path)
-print(client.list_deployments())
-print(client.get_deployment("add5"))
+try:
+    client.create_deployment("addN", model_5_path)
+    print(client.list_deployments())
+    print(client.get_deployment("addN"))
 
-model_input = pd.DataFrame([range(10)])
-model_output = client.predict("add5", model_input)
-assert model_output.equals(pd.DataFrame([range(5, 15)]))
+    model_input = pd.DataFrame([range(10)])
+    model_output = client.predict("addN", model_input)
+    assert model_output.equals(pd.DataFrame([range(5, 15)]))
 
-client.delete_deployment("add5")
-shutil.rmtree(model_path)
+    client.update_deployment("addN", model_uri=model_6_path, config={"model_traffic": 1.0})
+    print(client.get_deployment("addN"))
+
+    model_input = pd.DataFrame([range(10)])
+    model_output = client.predict("addN", model_input)
+    assert model_output.equals(pd.DataFrame([range(6, 16)]))
+finally:
+    client.delete_deployment("addN")
+    shutil.rmtree(os.path.dirname(model_5_path))
